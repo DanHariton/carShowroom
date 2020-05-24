3327491e3b12a175dabcb0d6e67a93a5086d6ac6
DanHariton <danilhariton@gmail.com>
2020-05-24 14:51:14 +0200

    search cars page

--- templates/src/page/js/app.js
+++ templates/src/page/js/app.js
@@ -1,1 +1,7 @@
-console.log('page app');
+window.$ = window.jQuery = require('jquery/dist/jquery');
+require('bootstrap');
+require('select2');
+
+$('select').select2({
+    theme: 'bootstrap4',
+});
