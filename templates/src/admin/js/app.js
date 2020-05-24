cd3c471075beebd2910727fbde1cdeb3c86ca570
SergiuszN <serg.ni2011@gmail.com>
2020-05-24 10:22:57 +0200

    init

---
+++ templates/src/admin/js/app.js
@@ -0,0 +1,4 @@
+window.$ = window.jQuery = require('jquery/dist/jquery.slim');
+require('startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle');
+require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing');
+require('startbootstrap-sb-admin-2/js/sb-admin-2');
